f6e97dbc05177cd5d2336ccaeb40969a4b39e5ed
Daniel <dan_rigg11@hotmail.com>
2018-08-06 17:01:52 +0100

    GPN 1.2
    Deleted animations for now.
    created movement script

---
+++ Level1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody myRigidbody;
+
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+
+	private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody>();
+		mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+		moveVelocity = moveInput * moveSpeed;
+
+		Ray cameraRay = mainCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayLength;
+
+		if (groundPlane.Raycast (cameraRay, out rayLength)) 
+		{
+			Vector3 pointToLook = cameraRay.GetPoint (rayLength);
+			Debug.DrawLine (cameraRay.origin, pointToLook, Color.blue);
+
+			transform.LookAt (pointToLook);
+		}
+	}
+
+	void FixedUpdate (){
+		myRigidbody.velocity = moveVelocity;
+	}
+}
