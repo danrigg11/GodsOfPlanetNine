15b1e2103e265e360de02bd88c49ffe793bea02e
Daniel <dan_rigg11@hotmail.com>
2018-09-05 09:47:14 +0100

    started menu page

---
+++ Level1/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	public Text highscore;
+
+	// Use this for initialization
+	void Start () {
+		highscore.text = PlayerPrefs.GetFloat ("BestTimeMin", 0) + ":" + PlayerPrefs.GetFloat ("BestTimeSec", 0).ToString("f2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Play()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Application.LoadLevel(2);
+	}
+}
