ab9b407f4216818c15bb628b7b854c4223e50e2b
Daniel <dan_rigg11@hotmail.com>
2018-09-05 16:07:57 +0100

    started coins counter script

---
+++ Level1/Assets/Scripts/coinCounter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class coinCounter : MonoBehaviour {
+	
+	public int coins;
+	public TextMeshProUGUI Counter;
+
+	// Use this for initialization
+	void Start () {
+		coins = 0;
+		Counter.text = coins + "/6";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
